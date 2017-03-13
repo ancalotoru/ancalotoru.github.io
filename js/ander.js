0300ce69132c72aa319756f91f9b1ae0a0476fbb
ancalotoru <ancalotoru@gmail.com>
2017-03-13 21:49:44 +0100

    nuevo archivo: img/portfolio/Dissertation_cover.png 	nuevo archivo: img/portfolio/Dissertation_cover_small.png 	borrado:       img/portfolio/dreams-preview.png 	borrado:       img/portfolio/dreams.png 	borrado:       img/portfolio/escape-preview.png 	borrado:       img/portfolio/escape.png 	nuevo archivo: img/portfolio/fiabiliapp-web.png 	nuevo archivo: img/portfolio/fiabiliapp.png 	borrado:       img/portfolio/golden-preview.png 	borrado:       img/portfolio/golden.png 	nuevo archivo: img/portfolio/perdiendo_los_papeles.png 	borrado:       img/portfolio/roundicons-free.png 	borrado:       img/portfolio/roundicons.png 	borrado:       img/portfolio/startup-framework-preview.png 	borrado:       img/portfolio/startup-framework.png 	borrado:       img/portfolio/treehouse-preview.png 	borrado:       img/portfolio/treehouse.png 	modificado:    index.html 	nuevo archivo: js/ander.js

---
+++ js/ander.js
@@ -0,0 +1,7 @@
+$('.about-dropdown-btn').on('click', function(){
+	$('#study', '#work', '#research').hide('slow');
+});
+
+$('.about-dropdown-btn').on('click', function(){
+	$('#study').show('slow');
+});
