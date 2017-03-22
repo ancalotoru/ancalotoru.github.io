2a19729e064cd7afbdb04adbbb58b5ed03e999d6
ancalotoru <ancalotoru@gmail.com>
2017-03-22 12:53:35 +0100

    modificado:    css/agency.css 	modificado:    css/timeline/style.css 	modificado:    index.html 	nuevo archivo: js/.Rhistory 	modificado:    js/ander.js 	modificado:    js/contact_me.js 	renombrado:    Resume.pdf -> resources/Resume.pdf 	nuevo archivo: resources/thesis_tfg.pdf

--- js/ander.js
+++ js/ander.js
@@ -1,5 +1,7 @@
-$(document).ready(function(){
-	$('.about-dropdown-btn').on('click', function(e){
-		$('#study').show('slow');
-	});
+$('.about-dropdown-btn').on('click', function(e){
+	var id = e.currentTarget.id;
+	$('#' + id.substr(0, id.length-2)).show('fast');
+});
+$(".trigger").click(function() {
+	$(".menu").toggleClass("active"); 
 });
--- js/contact_me.js
+++ js/contact_me.js
@@ -47,7 +47,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+                    $('#success > .alert-danger').append($("<strong>").html("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later! <br> Anyway, my email is: <a href='mailto:ander.carreno.lopez@gmail.com'><font color='black'><u>ancalotoru@gmail.com</u></font></a>"));
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
