654697c6e277f480675071623fc1e39bb8598f33
ancalotoru <ancalotoru@gmail.com>
2017-11-18 17:13:20 +0100

    Contact section changed

--- js/ander.js
+++ js/ander.js
@@ -1,7 +1,25 @@
+(function ($) {
+	$.each(['show', 'hide'], function (i, ev) {
+		var el = $.fn[ev];
+		$.fn[ev] = function () {
+			console.log('entra aqui');
+			this.trigger(ev);
+			return el.apply(this, arguments);
+		};
+	});
+})(jQuery)
+
 $('.about-dropdown-btn').on('click', function(e){
 	var id = e.currentTarget.id;
 	$('#' + id.substr(0, id.length-2)).show('fast');
 });
 $(".trigger").click(function() {
 	$(".menu").toggleClass("active"); 
+});
+
+window.sr = ScrollReveal({ reset: true});
+
+$('#test').on('show', function(){
+	console.log('funciona');
+	sr.reveal('#test', {duration: 2000, origin:'right'});
 });
